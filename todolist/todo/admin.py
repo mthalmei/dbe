909d5dcfd50e40b6e806a610714ced76bf7c4a77
Michael Thalmeier <michael@thalmeier.at>
2010-08-03 10:41:25 +0200

    modified template for todo item

--- todolist/todo/admin.py
+++ todolist/todo/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from todolist.todo.models import Item
+from todolist.todo.models import Item, DateTime
 
 class ItemAdmin(admin.ModelAdmin):
     list_display = ["name", "priority", "difficulty", "created", "done"]
     search_fields = ["name"]
 
+class ItemInline(admin.TabularInline):
+    model = Item
+
+class DateAdmin(admin.ModelAdmin):
+    list_display = ["datetime"]
+    inlines = [ItemInline]
+
 admin.site.register(Item, ItemAdmin)
+admin.site.register(DateTime, DateAdmin)
