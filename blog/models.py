82c96192cb1af8d1fd4b37824538e284bf25afa8
Michael Thalmeier <michael@thalmeier.at>
2010-08-04 15:23:16 +0200

    add comment notification

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.mail import send_mail
 
 class Post(models.Model):
     title  = models.CharField(max_length=60)
@@ -16,3 +17,17 @@ class Comment(models.Model):
 
     def __unicode__(self):
         return unicode("%s: %s" % (self.post, self.body[:60]))
+
+    def save(self, *args, **kwargs):
+        """Email when a comment is added."""
+        if "notify" in kwargs and kwargs["notify"] == True:
+            message = "Comment was added to '%s' by '%s': \n\n%s" % (self.post, self.author,
+                                                                         self.body)
+            from_addr = "no-reply@example.com"
+            recipient_list = ["myemail@domain.com"]
+            send_mail( "New comment added", message, from_addr, recipient_list)
+
+        if "notify" in kwargs: del kwargs["notify"]
+
+        super(Comment, self).save(*args, **kwargs)
+
