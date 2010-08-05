b3f72ee150ca24a86841376a10ccfb89af80e2c2
Michael Thalmeier <michael@thalmeier.at>
2010-08-05 13:26:51 +0200

    add album view

--- photo/urls.py
+++ photo/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('dbe.photo.views',
    (r"^$", "main"),
+   (r"^(\d+)/$", "album"),
 )
 
--- photo/views.py
+++ photo/views.py
@@ -27,3 +27,21 @@ def main(request):
 
     return render_to_response("photo/list.html", dict(albums=albums, user=request.user))
 
+def album(request, pk):
+    """Album listing."""
+    album = Album.objects.get(pk=pk)
+    if not album.public and not request.user.is_authenticated():
+        return HttpResponse("Error: you need to be logged in to view this album.")
+
+    images = album.image_set.all()
+    paginator = Paginator(images, 30)
+    try: page = int(request.GET.get("page", '1'))
+    except ValueError: page = 1
+
+    try:
+        images = paginator.page(page)
+    except (InvalidPage, EmptyPage):
+        images = paginator.page(paginator.num_pages)
+
+    return render_to_response("photo/album.html", dict(album=album, images=images, user=request.user))
+
